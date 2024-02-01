33407c633809dc562e6a5c021be2e05847488662
Jimmy Shong <jimmysh341@gmail.com>
2024-02-01 02:51:30 -0500

    made changes for EmotiVoice

--- frontend_en.py
+++ frontend_en.py
@@ -1,11 +1,11 @@
 # Copyright 2023, YOUDAO
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,8 @@ from g2p_en import G2p
 import os
 
 
-ROOT_DIR = os.path.dirname(os.path.abspath("__file__"))
+ROOT_DIR = os.path.dirname(os.path.abspath("__file__")) + "/EmotiVoice"
+
 
 def read_lexicon(lex_path):
     lexicon = {}
@@ -35,17 +36,19 @@ def read_lexicon(lex_path):
                 lexicon[word.lower()] = phones
     return lexicon
 
+
 def get_eng_phoneme(text, g2p, lexicon, pad_sos_eos=True):
     """
     english g2p
     """
     filters = {",", " ", "'"}
     phones = []
-    words = list(filter(lambda x: x not in {"", " "}, re.split(r"([,;.\-\?\!\s+])", text)))
+    words = list(
+        filter(lambda x: x not in {"", " "}, re.split(r"([,;.\-\?\!\s+])", text))
+    )
 
     for w in words:
         if w.lower() in lexicon:
-            
             for ph in lexicon[w.lower()]:
                 if ph not in filters:
                     phones += ["[" + ph + "]"]
@@ -53,12 +56,11 @@ def get_eng_phoneme(text, g2p, lexicon, pad_sos_eos=True):
             if "sp" not in phones[-1]:
                 phones += ["engsp1"]
         else:
-            phone=g2p(w)
+            phone = g2p(w)
             if not phone:
                 continue
 
             if phone[0].isalnum():
-                
                 for ph in phone:
                     if ph not in filters:
                         phones += ["[" + ph + "]"]
@@ -67,7 +69,7 @@ def get_eng_phoneme(text, g2p, lexicon, pad_sos_eos=True):
             elif phone == " ":
                 continue
             elif phones:
-                phones.pop() # pop engsp1
+                phones.pop()  # pop engsp1
                 phones.append("engsp4")
     if phones and "engsp" in phones[-1]:
         phones.pop()
@@ -76,21 +78,22 @@ def get_eng_phoneme(text, g2p, lexicon, pad_sos_eos=True):
     if pad_sos_eos:
         phones = ["<sos/eos>"] + phones + ["<sos/eos>"]
     return " ".join(phones)
-    
+
 
 if __name__ == "__main__":
     lexicon = read_lexicon(f"{ROOT_DIR}/lexicon/librispeech-lexicon.txt")
     g2p = G2p()
-    phonemes= get_eng_phoneme("Happy New Year", g2p, lexicon)
+    phonemes = get_eng_phoneme("Happy New Year", g2p, lexicon)
     import sys
     from os.path import isfile
+
     if len(sys.argv) < 2:
         print("Usage: python %s <text>" % sys.argv[0])
         exit()
     text_file = sys.argv[1]
     if isfile(text_file):
-        fp = open(text_file, 'r')
+        fp = open(text_file, "r")
         for line in fp:
-            phoneme=get_eng_phoneme(line.rstrip(), g2p, lexicon)
+            phoneme = get_eng_phoneme(line.rstrip(), g2p, lexicon)
             print(phoneme)
         fp.close()
--- inference_am_vocoder_joint.py
+++ inference_am_vocoder_joint.py
@@ -1,11 +1,11 @@
 # Copyright 2023, YOUDAO
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ import soundfile as sf
 from yacs import config as CONFIG
 from tqdm import tqdm
 
+
 def get_style_embedding(prompt, tokenizer, style_encoder):
     prompt = tokenizer([prompt], return_tensors="pt")
     input_ids = prompt["input_ids"]
@@ -30,19 +31,20 @@ def get_style_embedding(prompt, tokenizer, style_encoder):
 
     with torch.no_grad():
         output = style_encoder(
-        input_ids=input_ids,
-        token_type_ids=token_type_ids,
-        attention_mask=attention_mask,
-    )
+            input_ids=input_ids,
+            token_type_ids=token_type_ids,
+            attention_mask=attention_mask,
+        )
     style_embedding = output["pooled_output"].cpu().squeeze().numpy()
     return style_embedding
+
 
 def main(args, config):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     root_path = os.path.join(config.output_directory, args.logdir)
-    ckpt_path = os.path.join(root_path,  "ckpt")
+    ckpt_path = os.path.join(root_path, "ckpt")
     files = os.listdir(ckpt_path)
-    
+
     for file in files:
         if args.checkpoint:
             if file != args.checkpoint:
@@ -50,43 +52,38 @@ def main(args, config):
 
         checkpoint_path = os.path.join(ckpt_path, file)
 
-        with open(config.model_config_path, 'r') as fin:
+        with open(config.model_config_path, "r") as fin:
             conf = CONFIG.load_cfg(fin)
-        
-     
+
         conf.n_vocab = config.n_symbols
         conf.n_speaker = config.speaker_n_labels
 
         style_encoder = StyleEncoder(config)
         model_CKPT = torch.load(config.style_encoder_ckpt, map_location="cpu")
         model_ckpt = {}
-        for key, value in model_CKPT['model'].items():
+        for key, value in model_CKPT["model"].items():
             new_key = key[7:]
             model_ckpt[new_key] = value
         style_encoder.load_state_dict(model_ckpt, strict=False)
 
-
-
         generator = JETSGenerator(conf).to(device)
 
         model_CKPT = torch.load(checkpoint_path, map_location=device)
-        generator.load_state_dict(model_CKPT['generator'])
+        generator.load_state_dict(model_CKPT["generator"])
         generator.eval()
 
-        with open(config.token_list_path, 'r') as f:
-            token2id = {t.strip():idx for idx, t, in enumerate(f.readlines())}
+        with open(config.token_list_path, "r") as f:
+            token2id = {t.strip(): idx for idx, t, in enumerate(f.readlines())}
 
-        with open(config.speaker2id_path, encoding='utf-8') as f:
-            speaker2id = {t.strip():idx for idx, t in enumerate(f.readlines())}
-
+        with open(config.speaker2id_path, encoding="utf-8") as f:
+            speaker2id = {t.strip(): idx for idx, t in enumerate(f.readlines())}
 
         tokenizer = AutoTokenizer.from_pretrained(config.bert_path)
-        
+
         text_path = args.test_file
 
-   
-        if os.path.exists(root_path + "/test_audio/audio/" +f"{file}/"):
-            r = glob.glob(root_path + "/test_audio/audio/" +f"{file}/*")
+        if os.path.exists(root_path + "/test_audio/audio/" + f"{file}/"):
+            r = glob.glob(root_path + "/test_audio/audio/" + f"{file}/*")
             for j in r:
                 os.remove(j)
         texts = []
@@ -100,9 +97,10 @@ def main(args, config):
                 prompts.append(line[1])
                 texts.append(line[2].split())
                 contents.append(line[3])
-                
-        for i, (speaker, prompt, text, content) in enumerate(tqdm(zip(speakers, prompts, texts, contents))):
 
+        for i, (speaker, prompt, text, content) in enumerate(
+            tqdm(zip(speakers, prompts, texts, contents))
+        ):
             style_embedding = get_style_embedding(prompt, tokenizer, style_encoder)
             content_embedding = get_style_embedding(content, tokenizer, style_encoder)
 
@@ -111,48 +109,66 @@ def main(args, config):
             speaker = speaker2id[speaker]
 
             text_int = [token2id[ph] for ph in text]
-            
-            sequence = torch.from_numpy(np.array(text_int)).to(device).long().unsqueeze(0)
+
+            sequence = (
+                torch.from_numpy(np.array(text_int)).to(device).long().unsqueeze(0)
+            )
             sequence_len = torch.from_numpy(np.array([len(text_int)])).to(device)
             style_embedding = torch.from_numpy(style_embedding).to(device).unsqueeze(0)
-            content_embedding = torch.from_numpy(content_embedding).to(device).unsqueeze(0)
+            content_embedding = (
+                torch.from_numpy(content_embedding).to(device).unsqueeze(0)
+            )
             speaker = torch.from_numpy(np.array([speaker])).to(device)
             with torch.no_grad():
-
                 infer_output = generator(
-                        inputs_ling=sequence,
-                        inputs_style_embedding=style_embedding,
-                        input_lengths=sequence_len,
-                        inputs_content_embedding=content_embedding,
-                        inputs_speaker=speaker,
-                        alpha=1.0
+                    inputs_ling=sequence,
+                    inputs_style_embedding=style_embedding,
+                    input_lengths=sequence_len,
+                    inputs_content_embedding=content_embedding,
+                    inputs_speaker=speaker,
+                    alpha=1.0,
+                )
+                audio = infer_output["wav_predictions"].squeeze() * MAX_WAV_VALUE
+                audio = audio.cpu().numpy().astype("int16")
+                if not os.path.exists(root_path + "/test_audio/audio/" + f"{file}/"):
+                    os.makedirs(
+                        root_path + "/test_audio/audio/" + f"{file}/", exist_ok=True
                     )
-                audio = infer_output["wav_predictions"].squeeze()* MAX_WAV_VALUE
-                audio = audio.cpu().numpy().astype('int16')
-                if not os.path.exists(root_path + "/test_audio/audio/" +f"{file}/"):
-                    os.makedirs(root_path + "/test_audio/audio/" +f"{file}/", exist_ok=True)
-                sf.write(file=root_path + "/test_audio/audio/" +f"{file}/{i+1}.wav", data=audio, samplerate=config.sampling_rate) #h.sampling_rate
+                sf.write(
+                    file=root_path + "/test_audio/audio/" + f"{file}/{i+1}.wav",
+                    data=audio,
+                    samplerate=config.sampling_rate,
+                )  # h.sampling_rate
 
 
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("run!")
     p = argparse.ArgumentParser()
-    p.add_argument('-d', '--logdir', type=str, required=True)
+    p.add_argument("-d", "--logdir", type=str, required=True)
     p.add_argument("-c", "--config_folder", type=str, required=True)
-    p.add_argument("--checkpoint", type=str, required=False, default='', help='inference specific checkpoint, e.g --checkpoint checkpoint_230000')
-    p.add_argument('-t', '--test_file', type=str, required=True, help='the absolute path of test file that is going to inference')
+    p.add_argument(
+        "--checkpoint",
+        type=str,
+        required=False,
+        default="",
+        help="inference specific checkpoint, e.g --checkpoint checkpoint_230000",
+    )
+    p.add_argument(
+        "-t",
+        "--test_file",
+        type=str,
+        required=True,
+        help="the absolute path of test file that is going to inference",
+    )
 
-    args = p.parse_args() 
+    args = p.parse_args()
     ##################################################
-    sys.path.append(os.path.dirname(os.path.abspath("__file__")) + "/" + args.config_folder)
+    sys.path.append(
+        os.path.dirname(os.path.abspath("__file__")) + "/" + args.config_folder
+    )
 
     from config import Config
+
     config = Config()
     ##################################################
     main(args, config)
-
-
